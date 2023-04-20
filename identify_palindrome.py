e1f250024f3f021ae23398dc618e9ccb0fb1ba78
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-20 21:02:17 +0000

    Updated lowercase string handling

--- identify_palindrome.py
+++ identify_palindrome.py
@@ -3,12 +3,11 @@
 def is_palindrome(input_str):
     """This function determines if a string is a palindrom and prints the input and whether
      or not it passes the Boolean test of is or is not a palindrome"""
+    #lower the string
+    input_str_low = input_str.lower()
     #Reverse the string
-    #input_str = input("Please enter a string:")
-    #print(input_str)
-    #print(input_str[::-1])
-    reverse_str = input_str[::-1]
-    if input_str == reverse_str:
+    reverse_str = input_str_low[::-1]
+    if input_str_low == reverse_str:
         print ("[" + input_str + "] is a Palindrome!")
     else:
         print ("[" + input_str + "] is not a Palindrome!")
