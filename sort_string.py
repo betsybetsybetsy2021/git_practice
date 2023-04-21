6cdd3c3d1615a3158da854472344ab39a3f515df
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-21 17:40:46 +0000

    updating

--- sort_string.py
+++ sort_string.py
@@ -1,12 +1,12 @@
 """ This is the third in the LinkedIn warm-up exercises."""
 
-#def sort_string(string):
-"""This function is intended to split a space delimited string, organize the words 
+def sort_string(string):
+    """This function is intended to split a space delimited string, organize the words 
     alphabetically and return the sorted list of words in the original string."""
-string = 'The rain in Spain falls mainly on the plain.'
-print(stri)
-string_list = string.split()
-print (string_list)
-print(string_list.sort())
+    string = 'The rain in Spain falls mainly on the plain.'
+    print(string)
+    string_list = string.split()
+    print (string_list)
+    print(string_list.sort())
 
-#sort_string('The rain in Spain falls mainly on the plain.')
+sort_string('The rain in Spain falls mainly on the plain.')
