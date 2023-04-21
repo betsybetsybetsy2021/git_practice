b1786a2b4579f7148d32d326e66976ed57c98b34
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-21 19:02:38 +0000

    Corrected to preserve case

--- sort_string.py
+++ sort_string.py
@@ -1,13 +1,16 @@
 """ This is the third in the LinkedIn warm-up exercises."""
 
+#import re
+
 def sort_string(string):
     """This function is intended to split a space delimited string, organize the words 
     alphabetically and return the sorted list of words in the original string."""
-    string = 'The rain in Spain falls mainly on the plain.'
-    print(string)
+    string_list = []
     string_list = string.split()
-    print (type(string_list))
-    string_list.sort()
+    string_list.sort(key = lambda x: x.lower())
+    #new = re.split(r'\s', string) #This uses regular expressions
+    # but I'm not how to IgnoreCase to sort without changing
     print(string_list)
+
 
 sort_string('The rain in Spain falls mainly on the plain.')
