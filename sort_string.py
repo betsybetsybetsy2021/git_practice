65bff9df25d6b9662308a134913c72692699eb98
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-21 17:13:55 +0000

    April 21 Warm-up

---
+++ sort_string.py
@@ -0,0 +1,10 @@
+""" This is the third in the LinkedIn warm-up exercises."""
+
+def sort_string(string):
+    """This function is intended to split a space delimited string, organize the words 
+    alphabetically and return the sorted list of words in the original string."""
+    string_list = string.split()
+    print (string_list)
+    print(string_list.sort())
+
+sort_string('The rain in Spain falls mainly on the plain.')
