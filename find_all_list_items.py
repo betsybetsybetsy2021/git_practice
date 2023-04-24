8b84151fc675d512bd0cdbaedeec4af6b1130258
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-24 22:45:14 +0000

    creating file

--- find_all_list_items.py
+++ find_all_list_items.py
@@ -1,6 +1,14 @@
 """This is the LinkedIn Challenge 4.  To write a function that will find all values in a 
 mulit-level list"""
 
-example = [[1,2,3], 2, [1,3], [[1,2,3]]
+example = [[1,2,3], 2, [1,3], [1,2,3]]
+index_list = []
 
-example.index('2')
+print(type(example))
+print(type(example[0]))
+
+example.index(2) # This is the python built-in which cannot
+# find values in non-flat lists
+
+for i in len(example):
+    index_list.append(i)
