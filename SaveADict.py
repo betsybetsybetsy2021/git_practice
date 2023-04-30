644cb6432bad33a8aa15187ab92bb1917a88a2e3
MOSFETs not FETs <127641120+betsybetsybetsy2021@users.noreply.github.com>
2023-04-30 14:35:22 -0700

    File write works, file read needs error handling
    Function to write to file works properly.  Function to read file and convert the read string formatted as a dictionary works, but needs error handling.

--- SaveADict.py
+++ SaveADict.py
@@ -4,6 +4,7 @@ import json
 import os.path
 
 def create_dict():
+    """This function writes the contents to the user input file path and file name."""
     contents = {'one':1, 'two':2, 'three':3}
     print(type(contents))
     print("Please enter the destination file path and name : ") #\n<For example C:\Users\User1\myfile.txt>")
@@ -15,7 +16,19 @@ def create_dict():
     return
 
 
-def sort_string():
+def retrieve_object():
+    """ This function currently opens the file and returns the text file as a string and then converts it to a dictionary. """
+    print("Please enter the file path and name of the dictionary file to open:")
+    file_name = input("(opening files that are not formatted as dictionaries will cause errors): ")
+    file2 = open(file_name, "r")
+    file2_string = file2.read()
+    file2.close()
+    print("This is the string read from file: " + file2_string)
+    new_dict = eval(file2_string) #If this isn't a dictionary, this will throw errors.
+    #print("This is the results of the eval")
+    #print(type(new_dict))
+    print(new_dict)
     return
 
 create_dict()
+retrieve_object()
